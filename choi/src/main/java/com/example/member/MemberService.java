f0de0b664f1641329db460d66f98af4af3034c75
yoon940514 <117021432+BackEnd-Stu@users.noreply.github.com>
2022-11-08 11:41:14 +0900

    Delete choi directory

--- choi/src/main/java/com/example/member/MemberService.java
+++
@@ -1,6 +0,0 @@
-package com.example.member;
-
-public interface MemberService {
-	void join(MemberDto memberDto);
-	MemberDto findMember(Long memberId);
-}
--- choi/src/main/java/com/example/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.member.MemberDto;
-
-public interface MemberRepository {
-	void save (MemberDto member);
-	MemberDto findById(Long memberId);
-}
