49c4076f59b992fbd74b5007bba4fda665ff1551
choiyoon <yoon94514@gmail.com>
2022-12-20 17:31:24 +0900

    SOLID

---
+++ Choi/SOLID/src/main/java/com/example/solid/LSP/Rectangle.java
@@ -0,0 +1,10 @@
+package com.example.solid.LSP;
+
+public class Rectangle extends Shape {
+
+    public Rectangle(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+}
---
+++ Choi/SOLID/src/main/java/com/example/solid/LSP/Square.java
@@ -0,0 +1,10 @@
+package com.example.solid.LSP;
+
+public class Square extends Shape{
+
+    public Square(int length) {
+        setWidth(length);
+        setHeight(length);
+    }
+
+}
