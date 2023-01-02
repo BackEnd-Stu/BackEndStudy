00a7aa379ae7a9a77b2560ca377b4900952be8b3
yoon9 <yoon940514@naver.com>
2023-01-02 21:48:32 +0900

    23/01/02

--- Choi/Spring/src/main/java/com/springmvc/basic/requestmapping/MappingController.java
+++ Choi/Spring/src/main/java/com/springmvc/basic/requestmapping/MappingController.java
@@ -51,8 +51,7 @@ public class MappingController {
      * PathVariable 사용 다중
      */
     @GetMapping("/mapping/users/{userId}/orders/{orderId}")
-    public String mappingPath(@PathVariable String userId, @PathVariable Long
-            orderId) {
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
         log.info("mappingPath userId={}, orderId={}", userId, orderId);
         return "ok";
     }
